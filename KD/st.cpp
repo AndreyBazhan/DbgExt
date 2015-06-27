a5ac67d4cda00fa100052440c66c8163e3811cc5
Andrey Bazhan <info@andreybazhan.com>
2015-06-27 17:57:01 +0300

    Minor improvements

--- KD/st.cpp
+++ KD/st.cpp
@@ -32,7 +32,7 @@ Environment:
 --*/
 
 {
-    HRESULT Result;
+    HRESULT Status = S_OK;
     ULONG ProcessorType;
     ULONG PlatformId;
     ULONG Major;
@@ -51,109 +51,105 @@ Environment:
 
     UNREFERENCED_PARAMETER(args);
 
-    if ((Result = QueryInterfaces(DebugClient)) != S_OK) {
+    __try {
 
-        ReleaseInterfaces();
-        return Result;
-    }
+        if ((Status = QueryInterfaces(DebugClient)) != S_OK) {
 
-    if ((Result = DebugControl->GetActualProcessorType(&ProcessorType)) != S_OK) {
-
-        DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't get the processor type.\n");
-        ReleaseInterfaces();
-        return Result;
-    }
-
-    if ((Result = DebugControl->GetSystemVersion(&PlatformId, &Major, &Minor, NULL, NULL, NULL, &ServicePackNumber, NULL, NULL, NULL)) != S_OK) {
-
-        DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't get system version.\n");
-        ReleaseInterfaces();
-        return Result;
-    }
-
-    if ((Result = DebugSymbols->GetOffsetByName("nt!KeServiceDescriptorTable", &KeServiceDescriptorTable)) != S_OK) {
-
-        DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read nt!KeServiceDescriptorTable.\n");
-        ReleaseInterfaces();
-        return Result;
-    }
-
-    if ((Result = DebugSymbols->GetOffsetByName("nt!KiServiceLimit", &KiServiceLimit)) != S_OK) {
-
-        DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read nt!KiServiceLimit.\n");
-        ReleaseInterfaces();
-        return Result;
-    }
-
-    if ((Result = DebugDataSpaces->ReadPointersVirtual(1, KeServiceDescriptorTable, &ServiceTableBase)) != S_OK) {
-
-        DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read service table base.\n");
-        ReleaseInterfaces();
-        return Result;
-    }
-
-    if ((Result = DebugDataSpaces->ReadVirtual(KiServiceLimit, &Limit, sizeof(ULONG), &BytesRead)) != S_OK) {
-
-        DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read service table limit.\n");
-        ReleaseInterfaces();
-        return Result;
-    }
-
-    Address = ServiceTableBase;
-
-    DebugControl->Output(DEBUG_OUTPUT_NORMAL, "\n");
-
-    if (IMAGE_FILE_MACHINE_I386 == ProcessorType) {
-
-        for (i = 0; i < Limit; i++, Address += sizeof(ULONG)) {
-
-            ServiceName[0] = '\0';
-
-            if ((Result = DebugDataSpaces->ReadPointersVirtual(1, Address, &ServiceAddress)) != S_OK) {
-
-                DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read memory.\n");
-                ReleaseInterfaces();
-                return Result;
-            }
-
-            DebugSymbols->GetNameByOffset(ServiceAddress, (PSTR)ServiceName, _countof(ServiceName), &BytesRead, NULL);
-
-            DebugControl->Output(DEBUG_OUTPUT_NORMAL, "%03lx:\t%p\t%s\n", i, ServiceAddress, ServiceName);
+            __leave;
         }
-    }
-    else if (IMAGE_FILE_MACHINE_AMD64 == ProcessorType) {
 
-        for (i = 0; i < Limit; i++, Address += sizeof(ULONG)) {
+        if ((Status = DebugControl->GetActualProcessorType(&ProcessorType)) != S_OK) {
 
-            ServiceName[0] = '\0';
-
-            if ((Result = DebugDataSpaces->ReadVirtual(Address, &Offset, sizeof(Offset), &BytesRead)) != S_OK) {
-
-                DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read memory.\n");
-                ReleaseInterfaces();
-                return Result;
-            }
-
-            if (Minor < 6000) {
-
-                Offset &= ~0xF;
-            }
-            else {
-
-                Offset >>= 4;
-            }
-
-            ServiceAddress = ServiceTableBase + Offset;
-
-            DebugSymbols->GetNameByOffset(ServiceAddress, (PSTR)ServiceName, _countof(ServiceName), &BytesRead, NULL);
-
-            DebugControl->Output(DEBUG_OUTPUT_NORMAL, "%03lx:\t%p\t%s\n", i, ServiceAddress, ServiceName);
+            DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't get the processor type.\n");
+            __leave;
         }
+
+        if ((Status = DebugControl->GetSystemVersion(&PlatformId, &Major, &Minor, NULL, NULL, NULL, &ServicePackNumber, NULL, NULL, NULL)) != S_OK) {
+
+            DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't get system version.\n");
+            __leave;
+        }
+
+        if ((Status = DebugSymbols->GetOffsetByName("nt!KeServiceDescriptorTable", &KeServiceDescriptorTable)) != S_OK) {
+
+            DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read nt!KeServiceDescriptorTable.\n");
+            __leave;
+        }
+
+        if ((Status = DebugSymbols->GetOffsetByName("nt!KiServiceLimit", &KiServiceLimit)) != S_OK) {
+
+            DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read nt!KiServiceLimit.\n");
+            __leave;
+        }
+
+        if ((Status = DebugDataSpaces->ReadPointersVirtual(1, KeServiceDescriptorTable, &ServiceTableBase)) != S_OK) {
+
+            DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read service table base.\n");
+            __leave;
+        }
+
+        if ((Status = DebugDataSpaces->ReadVirtual(KiServiceLimit, &Limit, sizeof(ULONG), &BytesRead)) != S_OK) {
+
+            DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read service table limit.\n");
+            __leave;
+        }
+
+        Address = ServiceTableBase;
+
+        DebugControl->Output(DEBUG_OUTPUT_NORMAL, "\n");
+
+        if (IMAGE_FILE_MACHINE_I386 == ProcessorType) {
+
+            for (i = 0; i < Limit; i++, Address += sizeof(ULONG)) {
+
+                ServiceName[0] = '\0';
+
+                if ((Status = DebugDataSpaces->ReadPointersVirtual(1, Address, &ServiceAddress)) != S_OK) {
+
+                    DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read memory.\n");
+                    __leave;
+                }
+
+                DebugSymbols->GetNameByOffset(ServiceAddress, (PSTR)ServiceName, _countof(ServiceName), &BytesRead, NULL);
+
+                DebugControl->Output(DEBUG_OUTPUT_NORMAL, "%03lx:\t%p\t%s\n", i, ServiceAddress, ServiceName);
+            }
+        }
+        else if (IMAGE_FILE_MACHINE_AMD64 == ProcessorType) {
+
+            for (i = 0; i < Limit; i++, Address += sizeof(ULONG)) {
+
+                ServiceName[0] = '\0';
+
+                if ((Status = DebugDataSpaces->ReadVirtual(Address, &Offset, sizeof(Offset), &BytesRead)) != S_OK) {
+
+                    DebugControl->Output(DEBUG_OUTPUT_NORMAL, "Couldn't read memory.\n");
+                    __leave;
+                }
+
+                if (Minor < 6000) {
+
+                    Offset &= ~0xF;
+                }
+                else {
+
+                    Offset >>= 4;
+                }
+
+                ServiceAddress = ServiceTableBase + Offset;
+
+                DebugSymbols->GetNameByOffset(ServiceAddress, (PSTR)ServiceName, _countof(ServiceName), &BytesRead, NULL);
+
+                DebugControl->Output(DEBUG_OUTPUT_NORMAL, "%03lx:\t%p\t%s\n", i, ServiceAddress, ServiceName);
+            }
+        }
+
+        DebugControl->Output(DEBUG_OUTPUT_NORMAL, "\n");
+    }
+    __finally {
+
+        ReleaseInterfaces();
     }
 
-    DebugControl->Output(DEBUG_OUTPUT_NORMAL, "\n");
-
-    ReleaseInterfaces();
-
-    return S_OK;
+    return Status;
 }
